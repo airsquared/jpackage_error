19e23c8b6b00d83115d4890451af4f1579df8f3f
airsquared <36649395+airsquared@users.noreply.github.com>
2023-07-09 21:58:39 -0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 javac Main.java module-info.java
 jar cf error.jar Main.class module-info.class
-jpackage --verbose --input . --main-jar error.jar --main-class Main --add-launcher second=second.properties
+jpackage --verbose --input . --main-jar error.jar --main-class Main --add-launcher second=second.properties --module-path UNUSED
 
 rm Main.class module-info.class error.jar
